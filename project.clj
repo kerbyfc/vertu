9d4f33a5953dc04a26210d713b9c683a062fc75f
kerbyfc <kerbyfc@gmail.com>
2013-10-29 20:50:14 +0400

    update vertion

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vertu "0.1.0-SNAPSHOT"
+(defproject vertu "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
